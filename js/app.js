4446f418df5b8d3ad13ee663fd57314f2924ea57
Suriya Krishna.B.S <90369580+Suriya-Krishna@users.noreply.github.com>
2021-10-28 20:42:42 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,5 @@
+let sidebar_toggle= document.querySelector(".bi-list");
+sidebar_toggle.addEventListener("click", () =>{
+    let sidebar= document.querySelector(".sidebar");
+    sidebar.classList.toggle("sidebar-collapse");
+  });
